a70c31ad1a9ed25f052ab8497f03515266290a1b
syndikos <onsummit@outlook.com>
2017-06-02 23:08:58 +0800

    add list comprehension

--- all.py
+++ all.py
@@ -268,6 +268,21 @@ def snake(x):
     else:
         return y - x
     
+def divisors(n):
+    return [1] + [x for x in range(2, n) if n % x == 0]
+
+def width(area, height):
+    assert area % height == 0
+    return area // height
+
+def perimeter(width, height):
+    return 2 * width + 2 * height
+
+def minimum_perimeter(area):
+    heights = divisors(area)
+    perimeters = [perimeter(width(area, h), h) for h in heights]
+    return min(perimeters)
+
 
     
     
